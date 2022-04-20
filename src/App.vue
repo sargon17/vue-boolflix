723161c1505157cb6b2d74dc3a6c1e0064c269e8
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-04-20 12:15:55 +0200

    init + file system

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>HEllo World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+@import "styles/general.scss";
+
+#app {
+  width: 100%;
+  min-height: 100vh;
+  height: 100vh;
+  background: #141414;
+}
+</style>
